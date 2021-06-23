6ba67eb97d594448eb0068e57d960309c968ac42
lscantelbury <lhsda.snf20@uea.edu.br>
2021-06-23 15:55:01 -0400

    Bullet Dx & Dy

--- bullet.py
+++ bullet.py
@@ -2,12 +2,13 @@ import pygame
 
 class Bullet():
     
-    def __init__(self, parent, image, rect, speed, direction):
+    def __init__(self, parent, image, rect, speed, dx, dy):
         self.parent = parent
         self.image = image
         self.rect = rect
         self.speed = speed
-        self.direction = direction
+        self.dx = dx
+        self.dy = dy
 
     def update(self):
         pass
